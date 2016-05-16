a84b3e45243bdb4add1d4451a6a379ed54dfb163
Niku145 <nikunj.patel@safalinfotech.com>
2016-05-16 18:01:40 +0530

    kkk

--- www/js/app.js
+++ www/js/app.js
@@ -6,24 +6,24 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-        .run(function ($ionicPlatform) {
-            $ionicPlatform.ready(function () {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if (window.cordova && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                    cordova.plugins.Keyboard.disableScroll(true);
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-                }
-                if (window.StatusBar) {
-                    // org.apache.cordova.statusbar required
-                    StatusBar.styleDefault();
-                }
-            });
-        })
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
 
-        .config(function ($stateProvider, $urlRouterProvider) {
-            $stateProvider
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
 
                     .state('login', {
                         url: '/login',
@@ -49,4 +49,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
             // if none of the above states are matched, use this as the fallback
             $urlRouterProvider.otherwise('/login');
-        });
+  // if none of the above states are matched, use this as the fallback
+ 
+});
